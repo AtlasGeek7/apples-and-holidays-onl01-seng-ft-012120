1d5fb238717b1636dbc1da473f23356ca8124442
Khalid T <khalid.tissire@gmail.com>
2020-01-26 01:36:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash[:winter].map { |holiday, supplies|
-    supplies }
+    supplies }.flatten
 end
 
 def all_holidays_with_bbq(holiday_hash)
